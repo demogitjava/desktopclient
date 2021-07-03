4a036c4b4cbe3188eda34a9be75ae64dbdd0fa11
hoscho <jgeist@gmx.net>
2021-07-03 23:20:28 +0200

    add basic auth to JDesktopPane the auth is with a simple Base64 encode and decode to Auth String Aut* Basic dsafasdfsafsdf

--- src/main/java/de/jsoft/jdesktop/config/LoginProvider.java
+++ src/main/java/de/jsoft/jdesktop/config/LoginProvider.java
@@ -5,6 +5,8 @@
  */
 package de.jsoft.jdesktop.config;
 
+import de.jsoft.JDesktop;
+import de.jsoft.jdesktop.login.NewJInternalFrame;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,6 +16,7 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -48,10 +51,38 @@ public class LoginProvider
             client = HttpClientBuilder.create().setDefaultCredentialsProvider(provider).build();
             
             HttpResponse response = client.execute(
-                    new HttpGet(de.jsoft.JDesktop.baseUrl + "login"));
-            int statusCode = response.getStatusLine()
-                    .getStatusCode();
+                    new HttpPost(de.jsoft.JDesktop.baseUrl + "login"));
             
+            
+            int statusCode = response.getStatusLine().getStatusCode();
+            
+            System.out.print("status code is" + statusCode + "\n");
+            
+            
+             HttpResponse response1 = client.execute(
+                    new HttpGet(de.jsoft.JDesktop.baseUrl + "getUserData"));
+              int statusCode1 = response1.getStatusLine().getStatusCode();
+              if(statusCode1 == 200)
+              {
+                  System.out.print("login sucess" + "\n");
+                 
+                // close Login JInternalFrame
+                
+                de.jsoft.jdesktop.login.NewJInternalFrame loginframe = new de.jsoft.jdesktop.login.NewJInternalFrame();
+                JDesktop.jdeskpane.removeAll();
+             
+                // get customer JInternalPanel   
+                de.jsoft.jdesktop.kundenstamm.Kundenstramm kdstamm = new de.jsoft.jdesktop.kundenstamm.Kundenstramm();
+                kdstamm.setVisible(true);
+                kdstamm.setMaximizable(true);
+                kdstamm.pack();
+                JDesktop.jdeskpane.add(kdstamm);
+              }
+              else
+              {
+                  System.out.print("login failed");
+                  // username and password are wrong
+              }
             assertThat(statusCode, equalTo(HttpStatus.SC_OK));
         } catch (IOException ex) {
             Logger.getLogger(LoginProvider.class.getName()).log(Level.SEVERE, null, ex);
