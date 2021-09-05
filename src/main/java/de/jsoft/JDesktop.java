b5c75e86637e9228fa1a195521ab57d8cde9e43b
demogitjava <jgeist@gmx.net>
2021-09-05 15:22:50 +0200

    Initial commit

---
+++ src/main/java/de/jsoft/JDesktop.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.jsoft;
+
+
+import de.jsoft.jdesktop.mainframe.MainFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
+import javax.swing.UIManager.*;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import javax.swing.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.http.HttpHeaders;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = "de.jsoft")
+public class JDesktop 
+{
+    
+    public static MainFrame mframe;
+    public static RestTemplate rtemp;
+    
+    public static String baseUrl;
+    public static JDesktopPane jdeskpane;
+
+    public static int lgglobal;
+    public static boolean userislogedin = false;
+    
+    
+    public JDesktop()
+    {
+           
+    }
+    
+    public static Integer setLgtoPanel(int indexlanguage)
+    {
+       
+        lgglobal = 0;
+        lgglobal = indexlanguage;
+        return lgglobal;
+    }
+    
+    
+    public static void main(String[] args)
+    {
+
+
+       
+        /*
+        
+            Look and Feel 
+            setup to Nimbus
+        
+        */
+
+
+        try
+        {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e)
+        {
+            System.out.print("LookAndFeel Error Nimbus !");
+        }
+
+
+        baseUrl = "http://localhost:8443/";
+
+        JDesktop jdesk = new JDesktop();
+        rtemp = new RestTemplate();
+
+
+        mframe = new MainFrame();
+
+        jdeskpane = MainFrame.jDesktopPane1;
+
+        /*
+            inizialisiere Klassen fÃ¼r DeskopPane
+        */
+        Object size = (Object) MainFrame.jDesktopPane1.getSize();
+
+
+        de.jsoft.jdesktop.login.NewJInternalFrame loginframe = new de.jsoft.jdesktop.login.NewJInternalFrame();
+        loginframe.setVisible(true);
+
+             
+        jdeskpane.add(loginframe);
+
+        mframe.add(jdeskpane);
+       
+        mframe.setExtendedState(JFrame.MAXIMIZED_BOTH);  
+        mframe.setVisible(true);
+        // mframe.pack();
+    }
+    
+    
+ 
+    
+}
---
+++ src/main/java/de/jsoft/jdesktop/menu/WindowsMenu.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.jsoft.jdesktop.menu;
+
+import de.jsoft.JDesktop;
+
+import static de.jsoft.JDesktop.jdeskpane;
+import static de.jsoft.JDesktop.mframe;
+import java.awt.BorderLayout;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+import javax.swing.plaf.metal.DefaultMetalTheme;
+import javax.swing.plaf.synth.SynthLookAndFeel;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
+/**
+ *
+ * @author hoscho
+ */
+public class WindowsMenu 
+{
+    
+    public JMenuBar bar;
+    
+    public String txusername;
+    public int intport;
+
+    String stlaf;
+
+    public JMenuBar setMenu()
+    {
+         
+          bar = new JMenuBar(); //Constructing JMenuBar
+          JMenu menu = new JMenu("File"); //Constructing JMenu name "File"
+          
+         
+          
+          JMenuItem exitapplication = new JMenuItem("Exit"); //Constructing JMenuItem with "Add New Data" label
+          exitapplication.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent ev) {
+                System.exit(0);
+            }
+          });
+          
+     
+          menu.add(exitapplication); //Adding JMenuItem in the JMenu
+
+
+          /*
+                Settings
+           */
+          JMenu menusettings = new JMenu("Settings");
+
+          JMenuItem setlookandfeelmetal = new JMenuItem("MetalLAF");
+            setlookandfeelmetal.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent ev)
+                {
+                    stlaf = "Metal";
+                    try {
+                        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                            if ("Metal".equals(info.getName())) {
+                                UIManager.setLookAndFeel(info.getClassName());
+                                SwingUtilities.updateComponentTreeUI(mframe);
+                                break;
+                            }
+                        }
+                    } catch (Exception e) {
+                        // If Nimbus is not available, you can set the GUI to another look and feel.
+                    }
+
+                }
+            });
+           menusettings.add(setlookandfeelmetal);
+
+
+        JMenuItem setlookandfeelwindows = new JMenuItem("WindowsLAF");
+        setlookandfeelwindows.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {
+                stlaf = "Windows";
+                try {
+                    for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                        if ("Windows".equals(info.getName())) {
+                            UIManager.setLookAndFeel(info.getClassName());
+                            SwingUtilities.updateComponentTreeUI(mframe);
+                            break;
+                        }
+                    }
+                } catch (Exception e) {
+                    // If Nimbus is not available, you can set the GUI to another look and feel.
+                }
+
+
+            }
+        });
+        menusettings.add(setlookandfeelwindows);
+
+        JMenuItem setlookandfeelnimbus = new JMenuItem("Nimbus");
+        setlookandfeelnimbus.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {
+                stlaf = "Nimbus";
+                try {
+                    for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                        if ("Nimbus".equals(info.getName())) {
+                            UIManager.setLookAndFeel(info.getClassName());
+                            SwingUtilities.updateComponentTreeUI(mframe);
+                            break;
+                        }
+                    }
+                } catch (Exception e) {
+                    // If Nimbus is not available, you can set the GUI to another look and feel.
+                }
+
+
+            }
+        });
+        menusettings.add(setlookandfeelnimbus);
+
+
+        bar.add(menu); //Adding JMenu in the JMenuBar
+        bar.add(menusettings);
+
+     
+         
+        return bar;
+       
+    }
+    
+}
