4b9d772b9612d1cf4e4881e3c28acdabbbe7c2f6
demogitjava <jgeist@gmx.net>
2021-09-08 07:31:03 +0200

    hoscho
    edit kdstamm data

--- src/main/java/de/jsoft/jdesktop/kundenstamm/MKundenstamm.java
+++ src/main/java/de/jsoft/jdesktop/kundenstamm/MKundenstamm.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.Date;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
+import javax.persistence.*;
 /**
  *
  * @author hoscho
@@ -12,6 +12,8 @@ import javax.validation.constraints.Size;
 public class MKundenstamm {
 
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     private String kundennummer;
