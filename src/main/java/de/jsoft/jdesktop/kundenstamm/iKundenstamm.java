0a1a9471833be750cf70039abd0cde35dfeb9bd8
demogitjava <jgeist@gmx.net>
2021-12-17 05:38:49 +0100

    add interfaces to iKundenstamm and Login JInternalFrame

--- src/main/java/de/jsoft/jdesktop/kundenstamm/iKundenstamm.java
+++ src/main/java/de/jsoft/jdesktop/kundenstamm/iKundenstamm.java
@@ -1,6 +1,20 @@
 package de.jsoft.jdesktop.kundenstamm;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+
+import java.awt.event.ActionEvent;
+import java.util.List;
+
 public interface iKundenstamm
 {
+    public void updateJTextFields(ActionEvent aevent);
+    public ActionEvent getAevent();
+    public void setAevent(ActionEvent aevent);
+
+    public static List<ClientHttpRequestInterceptor> interceptors = null;
+    public static List<MKundenstamm> searchresult = null;
+    public static List<Artikelstamm> selecteditems = null;
+    public ActionEvent aevent = null;
 
 }
--- src/main/java/de/jsoft/jdesktop/login/iNewJInternalFrame.java
+++ src/main/java/de/jsoft/jdesktop/login/iNewJInternalFrame.java
@@ -1,6 +1,21 @@
 package de.jsoft.jdesktop.login;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+
 public interface iNewJInternalFrame
 {
+    public void setTexttoComponents();
+    public HttpHeaders setHeaders(String authStringEnc);
 
+    public static HttpHeaders headers = null;
+    public static HttpEntity<String> httpentity = null;
+
+    public static HttpHeaders header = null;
+    public static RestTemplateBuilder restTemplateBuilder = null;
+
+    String stlabel1 = null;
+    String stlabel2 = null;
+    String stlabel3 = null;
 }
